c4701d1bdbe0266eeadcf3eca13e624b4a6967dc
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-03-24 20:55:33 -1000

    complete with javascript

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const pass = document.querySelector('#password');
-const confirm = document.querySelector('#confirm-password');
+const confirmPass = document.querySelector('#confirm-password');
 const submit = document.querySelector('.submit');
+const field = document.querySelector('fieldset');
 
+submit.addEventListener('click', (e) => {
+        if(confirmPass.value != pass.value) {
+            pass.classList.add('error');
+            confirmPass.classList.add('error');
+            const div = document.createElement('div');
+            div.textContent = "passwords do not match";
+            div.classList.add('message');
+            field.appendChild(div);
+            
+            e.preventDefault();
+        }
+    })
 
-submit.addEventListener('click', () => {
-    if(pass.textContent != confirm.textContent) {
-        alert("passwords do not match");
-    }
-})
